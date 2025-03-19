7b768652cb429233c40caabcd2fc67b10b7586c7
Aeddan <aeddan.williams@imt-atlantique.net>
2025-03-19 12:13:57 +0100

    Controller creation

--- src/admin/AdminController.php
+++ src/admin/AdminController.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class LandingController extends AbstractController{
-    #[Route('/', name: 'app_landing')]
+final class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render('landing/index.html.twig', [
-            'controller_name' => 'LandingController',
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
         ]);
     }
 }
